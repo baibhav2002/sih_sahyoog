80f8e709d8310d9bc08202e4b4619f2d7eac05b6
Pawan Kumar <pawankumar49871@gmail.com>
2023-09-16 18:43:16 +0530

    FIX: error handling added

--- llm_model/app.py
+++ llm_model/app.py
@@ -2,19 +2,28 @@ from flask import Flask, request
 from keybert import KeyBERT
 from sentence_transformers import SentenceTransformer
 
-from llm_model.helper.helper import KeywordResponse
+from llm_model.helper.helper import ErrorResponse, KeywordResponse
 app = Flask(__name__)
 
 ##---------Kyeword Extraction Start-----------------##
 @app.post('/keyword-extractor')
 def keyword_extractor():
-    request_data = request.get_json()
-    doc = request_data['document']
-    sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
-    kw_model = KeyBERT(model=sentence_model)
-    keyword_1 = kw_model.extract_keywords(doc,top_n=10)
-    formatted_data = [item[0] for item in keyword_1] 
-    return KeywordResponse(formatted_data,200)
+    try:
+        request_data = request.get_json()
+        doc = request_data['document']
+        sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
+        kw_model = KeyBERT(model=sentence_model)
+        keyword_1 = kw_model.extract_keywords(doc,top_n=10)
+        formatted_data = [item[0] for item in keyword_1] 
+        return KeywordResponse(formatted_data,200)
+    
+    except KeyError as e:
+        error_message = f"KeyError: '{str(e)}'"
+        return ErrorResponse(error_message, 400)
+    
+    except Exception as e:
+        error_message = str(e)
+        return ErrorResponse(error_message, 500)
 ##---------Kyeword Extraction End-----------------##
 
 if __name__ == '__main__':
