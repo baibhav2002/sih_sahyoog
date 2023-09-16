263b4341c1d8fce486ee6ec74d28c00d21bd1613
Pawan Kumar <pawankumar49871@gmail.com>
2023-09-16 18:07:48 +0530

    ADD: Embeddings Extraction added

--- llm_model/app.py
+++ llm_model/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
-
+from keybert import KeyBERT
+from sentence_transformers import SentenceTransformer
 app = Flask(__name__)
 
 ##---------Kyeword Extraction Start-----------------##
@@ -7,7 +8,10 @@ app = Flask(__name__)
 def keyword_extractor():
     request_data = request.get_json()
     doc = request_data['document']
-    return doc
+    sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
+    kw_model = KeyBERT(model=sentence_model)
+    keyword_1 = kw_model.extract_keywords(doc,top_n=10)
+    return keyword_1
 ##---------Kyeword Extraction End-----------------##
 
 if __name__ == '__main__':
