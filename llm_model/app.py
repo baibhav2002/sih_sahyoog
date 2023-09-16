5e2b7841a1c3348299cb1aab5e976b42935a836b
Pawan Kumar <pawankumar49871@gmail.com>
2023-09-16 17:53:06 +0530

    ADD: keyword-extraction flow added

--- llm_model/app.py
+++ llm_model/app.py
@@ -2,10 +2,13 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.get('/predict')
-def predict():
-    return 'Hello World!'
-
+##---------Kyeword Extraction Start-----------------##
+@app.post('/keyword-extractor')
+def keyword_extractor():
+    request_data = request.get_json()
+    doc = request_data['document']
+    return doc
+##---------Kyeword Extraction End-----------------##
 
 if __name__ == '__main__':
     app(app, host='0.0.0.0', port=5000)
